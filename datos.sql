0dcbf5548902f47f6f13e773db7688783f912c27
gabo2084 <gabo2084@hotmail.com>
2019-06-04 09:23:42 -0300

    Tercer commit

--- datos.sql
+++ datos.sql
@@ -1,4 +1,4 @@
-//Datos para tabla pais
+/*Datos para tabla pais*/
 
 insert into pais
 values (null, 'Argentina'),
@@ -8,7 +8,7 @@ values (null, 'Argentina'),
 (null, 'Perú');
 
 
-//Datos para tabla provincia
+/*Datos para tabla provincia*/
 
 insert into provincia
 values (null, 1, 'Ciudad Autónoma de Buenos Aires'),
@@ -37,7 +37,7 @@ values (null, 1, 'Ciudad Autónoma de Buenos Aires'),
 (null, 1, 'Tucumán');
 
 
-//Datos para tabla localidad
+/*Datos para tabla localidad*/
 
 insert into localidad
 values (null, 1, 'Agronomía'),
@@ -88,3 +88,192 @@ values (null, 1, 'Agronomía'),
 (null, 1, 'Villa Santa Rita'),
 (null, 1, 'Villa Soldati'),
 (null, 1, 'Villa Urquiza');
+
+
+/*Datos para tabla localidad*/
+
+insert into localidad
+values (null, 2, 'Adolfo Alsina'),
+(null, 2, 'Adolfo Gonzales Chaves'),
+(null, 2, 'Alberti'),
+(null, 2, 'Almirante Brown'),
+(null, 2, 'Arrecifes'),
+(null, 2, 'Avellaneda'),
+(null, 2, 'Ayacucho'),
+(null, 2, 'Azul'),
+(null, 2, 'Bahía Blanca'),
+(null, 2, 'Balcarce'),
+(null, 2, 'Baradero'),
+(null, 2, 'Benito Juárez'),
+(null, 2, 'Berazategui'),
+(null, 2, 'Berisso'),
+(null, 2, 'Bolívar'),
+(null, 2, 'Bragado'),
+(null, 2, 'Brandsen'),
+(null, 2, 'Campana'),
+(null, 2, 'Cañuelas'),
+(null, 2, 'Capitán Sarmiento Carlos'),
+(null, 2, 'Carlos Casares'),
+(null, 2, 'Carlos Tejedor'),
+(null, 2, 'Carmen de Areco'),
+(null, 2, 'Castelliacabuco'),
+(null, 2, 'Chascomús'),
+(null, 2, 'Chivilcoy'),
+(null, 2, 'Colón'),
+(null, 2, 'Coronel de Marina Leonardo Rosales'),
+(null, 2, 'Coronel Dorrego'),
+(null, 2, 'Coronel Pringles'),
+(null, 2, 'Coronel Suárez'),
+(null, 2, 'Daireaux'),
+(null, 2, 'Dolores'),
+(null, 2, 'Ensenada'),
+(null, 2, 'Escobar'),
+(null, 2, 'Esteban Echeverría'),
+(null, 2, 'Exaltación de la Cruz'),
+(null, 2, 'Ezeiza'),
+(null, 2, 'Florencio Varela'),
+(null, 2, 'Florentino Ameghin'),
+(null, 2, 'General Alvarado'),
+(null, 2, 'General Alvear'),
+(null, 2, 'General Arenales'),
+(null, 2, 'General Belgrano'),
+(null, 2, 'General Guid'),
+(null, 2, 'General Juan Madariaga'),
+(null, 2, 'General La Madrid'),
+(null, 2, 'General Las Heras'),
+(null, 2, 'General Lavalle'),
+(null, 2, 'General Paz'),
+(null, 2, 'General Pinto'),
+(null, 2, 'General Pueyrredón'),
+(null, 2, 'General Rodríguez'),
+(null, 2, 'General San Martín'),
+(null, 2, 'General Viamonte'),
+(null, 2, 'General Villegas'),
+(null, 2, 'Guaminí	Partido'),
+(null, 2, 'Hipólito Yrigoyen'),
+(null, 2, 'Hurlingham'),
+(null, 2, 'Ituzaingó'),
+(null, 2, 'José C. Paz'),
+(null, 2, 'Junín'),
+(null, 2, 'La Costa'),
+(null, 2, 'La Matanza'),
+(null, 2, 'Lanús'),
+(null, 2, 'La Plata'),
+(null, 2, 'Laprida'),
+(null, 2, 'Las Flores'),
+(null, 2, 'Leandro N. Alem'),
+(null, 2, 'Lincoln'),
+(null, 2, 'Lobería'),
+(null, 2, 'Lobos'),
+(null, 2, 'Lomas de Zamora'),
+(null, 2, 'Luján'),
+(null, 2, 'Magdalena'),
+(null, 2, 'Maipú'),
+(null, 2, 'Malvinas Argentinas'),
+(null, 2, 'Mar Chiquita'),
+(null, 2, 'Marcos Paz'),
+(null, 2, 'Mercedes'),
+(null, 2, 'Merlo'),
+(null, 2, 'Monte'),
+(null, 2, 'Monte Hermoso'),
+(null, 2, 'Moreno'),
+(null, 2, 'Morón'),
+(null, 2, 'Navarro'),
+(null, 2, 'Necochea'),
+(null, 2, 'Nueve de Julio (9 de Julio)'),
+(null, 2, 'Olavarría'),
+(null, 2, 'Patagones'),
+(null, 2, 'Pehuajó'),
+(null, 2, 'Pellegrini'),
+(null, 2, 'Pergamino'),
+(null, 2, 'Pila'),
+(null, 2, 'Pilar'),
+(null, 2, 'Pinamar'),
+(null, 2, 'Presidente Perón'),
+(null, 2, 'Puan'),
+(null, 2, 'Punta Indio'),
+(null, 2, 'Quilmes'),
+(null, 2, 'Ramallo'),
+(null, 2, 'Rauch'),
+(null, 2, 'Rivadavia'),
+(null, 2, 'Rojas'),
+(null, 2, 'Roque Pérez'),
+(null, 2, 'Saavedra'),
+(null, 2, 'Saladillo'),
+(null, 2, 'Salliqueló'),
+(null, 2, 'Salto'),
+(null, 2, 'San Andrés de Giles'),
+(null, 2, 'San Antonio de Areco'),
+(null, 2, 'San Cayetano'),
+(null, 2, 'San Fernando'),
+(null, 2, 'San Isidro'),
+(null, 2, 'San Migue'),
+(null, 2, 'San Nicolás'),
+(null, 2, 'San Pedro'),
+(null, 2, 'San Vice'),
+(null, 2, 'Suipac'),
+(null, 2, 'Tandil'),
+(null, 2, 'Tapal'),
+(null, 2, 'Tigre'),
+(null, 2, 'Tordi'),
+(null, 2, 'Tornquist'),
+(null, 2, 'Trenque Lauquen'),
+(null, 2, 'Tres Arroyos'),
+(null, 2, 'Tres de Febrero'),
+(null, 2, 'Tres Lo'),
+(null, 2, 'Veinticinco de Mayo (25 de Mayo)'),
+(null, 2, 'Vicente López'),
+(null, 2, 'Villa Gesell'),
+(null, 2, 'Villarino'),
+(null, 2, 'Zárate'); 
+
+
+/*Datos para la tabla cliente*/
+
+insert into cliente
+values (null, 'Juan', 'Perez', '32456987', 'M', '1543216789', 'juanpe@hotmail.com'),
+(null, 'Pedro', 'Picapiedra', '31324521', 'M', '1543216789', 'pedropi@gmail.com'),
+(null, 'Pablo', 'Marmol', '25321648', 'M', '1543216789', 'pablomar@yahoo.com'),
+(null, 'Luciana', 'Romano', '29006357', 'F', '1543216789', 'lucianaro@hotmail.com'),
+(null, 'Micaela', 'Robles', '20777034', 'F', '1543216789', 'micaelaro@gmail.com');
+
+
+/*Datos para la tabla domicilio*/
+
+insert into domicilio
+values (null, 1, 'Rivadavia', 12000, 'PB', '', '1036'),
+(null, 1, 'La Plata', 157, '3', 'F', '1146'),
+(null, 1, 'Belgrano', 1756, '7', '33', '1215'),
+(null, 2, 'Cabildo', 480, '', '', '1551'),
+(null, 2, 'Triunvirato', 1200, '2', 'D', '1352'),
+(null, 1, 'Boedo', 1241, '4', 'H', '1051');
+
+/*Datos para la tabla cliente_domicilio*/
+
+insert into cliente_domicilio
+values (null, 1, 1),
+(null, 2, 2),
+(null, 3, 3),
+(null, 4, 4),
+(null, 5, 5),
+(null, 2, 6);
+
+
+/*Consultas para la tabla cliente_domicilio*/
+
+/*Obtener el id, nombre, apellido, calle y nro del domicilio del cliente*/
+
+select cd.id_cliente_domicilio, c.nombre, c.apellido, d.calle, d.nro
+from cliente_domicilio as cd
+inner join cliente as c
+on cd.id_cliente = c.id_cliente
+inner join domicilio as d
+on cd.id_domicilio = d.id_domicilio;
+
+
+
+
+on cd.id_cliente = c.id_cliente
+inner join domicilio as d
+on cd.id_domicilio = d.id_domicilio;
+
